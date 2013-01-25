d15774105ae1d706c706c85bbfebb42dbdc0cc19
Do Nhat Minh <mrordinaire@gmail.com>
2013-01-25 16:26:57 +0800

    added finalizer

--- ffmpeg/mediafile.go
+++ ffmpeg/mediafile.go
@@ -7,6 +7,7 @@ package ffmpeg
 import "C"
 import (
     "errors"
+    "runtime"
     "unsafe"
 )
 
@@ -16,22 +17,31 @@ type MediaFile struct {
 }
 
 func Open(name string) (*MediaFile, error) {
-    f := &MediaFile{Name: name}
+    file := &MediaFile{Name: name}
 
     cName := C.CString(name)
     defer C.free(unsafe.Pointer(cName))
 
-    if C.avformat_open_input(&f.fmtctx, cName, nil, nil) < 0 {
+    if C.avformat_open_input(&file.fmtctx, cName, nil, nil) < 0 {
         return nil, errors.New("cannot open file " + name)
     }
 
-    return f, nil
+    if C.avformat_find_stream_info(file.fmtctx, nil) < 0 {
+        file.Close()
+        return nil, errors.New("cannot find stream info for file " + name)
+    }
+
+    runtime.SetFinalizer(file, (*MediaFile).Close)
+
+    return file, nil
 }
 
-func (f *MediaFile) Close() error {
-    if f.fmtctx != nil {
-        C.avformat_close_input(&f.fmtctx)
+func (file *MediaFile) Close() error {
+    if file.fmtctx != nil {
+        C.avformat_close_input(&file.fmtctx)
     }
+
+    runtime.SetFinalizer(file, nil)
 
     return nil
 }
